94234faf1be79d78119c3a8569282d40c0cf1e75
edigonzales <edi.gonzales@gmail.com>
2022-02-25 16:08:38 +0100

    initial commit

---
+++ functionalTests/src/test/java/ch/so/agi/ipwvalidator/LibraryTest.java
@@ -0,0 +1,12 @@
+package ch.so.agi.ipwvalidator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+    //@Test 
+    void someLibraryMethodReturnsTrue() {
+//        Library classUnderTest = new Library();
+//        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    }
+}
