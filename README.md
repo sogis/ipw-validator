3fe8498c4b67fd8a0755a9955036e9dfd102652c
edigonzales <edi.gonzales@gmail.com>
2022-03-04 19:31:08 +0100

    add knoten tests

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # ipw-validator
 
 ## todo
-- BetreiberRef (o.ä.): see https://github.com/claeis/ilivalidator/issues/337
+- BetreiberRef (o.ä.): see https://github.com/claeis/ilivalidator/issues/337 -> @Disabled
 - Constraint gewünscht damit keine Organisationen geliefert werden?
 - Constraint gewünscht damit keine Sonderbauwerke geliefert werden?
-
+- Status: Probleme wegen ALL OF Issue. Falls Status vorhanden ist und unbekannt, wird keine Fehler gemeldet. see https://github.com/claeis/ilivalidator/issues/300 -> @Disabled
--- functionalTests/src/test/java/ch/so/agi/ipwvalidator/Knoten.java
+++ functionalTests/src/test/java/ch/so/agi/ipwvalidator/Knoten.java
@@ -135,6 +135,7 @@ public class Knoten {
         assertTrue(content.contains("Error: line 21: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY BetreiberRef"));
     }
     
+    @Disabled
     @Test
     public void Id_11005_ok(@TempDir Path tempDir) throws Exception {
         String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
@@ -266,5 +267,342 @@ public class Knoten {
         assertFalse(content.contains("Error"));
     }    
 
+    @Test
+    public void Id_11013_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11013/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11013/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11013/11013_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 22: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Funktion AND != unbekannt"));
+        assertTrue(content.contains("Error: line 37: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001002: MANDATORY Funktion AND != unbekannt"));
+    }
+    
+    @Test
+    public void Id_11013_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11013/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11013/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11013/11013_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }    
+    
+    @Test
+    public void Id_11014_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11014/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11014/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11014/11014_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 21: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY FunktionHierarchisch"));
+    }
+
+    @Test
+    public void Id_11014_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11014/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11014/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11014/11014_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }    
+
+    @Test
+    public void Id_11015_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11015/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11015/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11015/11015_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 21: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Lage"));
+    }
+
+    @Test
+    public void Id_11015_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11015/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11015/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11015/11015_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }   
+    
+    @Test
+    public void Id_11016_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11016/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11016/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11016/11016_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 21: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Lagegenauigkeit"));
+    }
+
+    @Test
+    public void Id_11016_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11016/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11016/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11016/11016_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }    
+
+    @Test
+    public void Id_11017_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11017/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11017/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11017/11017_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 22: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Nutzungsart_geplant AND != unbekannt"));
+        assertTrue(content.contains("Error: line 37: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001002: MANDATORY Nutzungsart_geplant AND != unbekannt"));
+    }
+
+    @Test
+    public void Id_11017_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11017/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11017/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11017/11017_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }    
+
+    @Test
+    public void Id_11018_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11018/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11018/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11018/11018_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 22: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Nutzungsart_Ist AND != unbekannt"));
+        assertTrue(content.contains("Error: line 37: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001002: MANDATORY Nutzungsart_Ist AND != unbekannt"));
+    }
+
+    @Test
+    public void Id_11018_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11018/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11018/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11018/11018_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }  
+    
+    @Test
+    public void Id_11024_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11024/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11024/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11024/11024_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 21: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Sohlenkote (gilt für PAA)"));
+    }
+
+    @Test
+    public void Id_11024_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11024/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11024/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11024/11024_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }    
+
+    @Disabled
+    @Test
+    public void Id_11025_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11025/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11025/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11025/11025_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 22: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Status AND != unbekannt"));
+    }
+
+    @Disabled
+    @Test
+    public void Id_11025_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11025/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11025/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11025/11025_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }    
+
+    @Test
+    public void Id_11028_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11028/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11028/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11028/11028_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 21: VSADSSMINI_2020_LV95.VSADSSMini.Knoten: tid deg5mQXX20001001: MANDATORY Zustandserhebung_Jahr"));
+    }
+
+    @Test
+    public void Id_11028_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"knoten/11028/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"knoten/11028/config.toml");
+        settings.setValue(Validator.SETTING_ALL_OBJECTS_ACCESSIBLE, Validator.TRUE);
+        
+        EhiLogger.getInstance().setTraceFilter(false);
+        
+        boolean valid = Validator.runValidation(TEST_IN+"knoten/11028/11028_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }    
 
 }
