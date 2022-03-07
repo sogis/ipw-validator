cb07a6f50db52f28e1df26f0b8287d6bdc1d530c
edigonzales <edi.gonzales@gmail.com>
2022-03-07 18:12:56 +0100

    add more leitung tests

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - Constraint gewünscht damit keine Organisationen geliefert werden?
 - Constraint gewünscht damit keine Sonderbauwerke geliefert werden?
 - Status: Probleme wegen ALL OF Issue. Falls Status vorhanden ist und unbekannt, wird keine Fehler gemeldet. see https://github.com/claeis/ilivalidator/issues/300 -> @Disabled
+- jreleaser...
--- functionalTests/src/test/java/ch/so/agi/ipwvalidator/Leitung.java
+++ functionalTests/src/test/java/ch/so/agi/ipwvalidator/Leitung.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.interlis2.validator.Validator;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -34,7 +35,6 @@ public class Leitung {
         assertTrue(content.contains("Error: line 39: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002002: MANDATORY Baujahr (gilt für PAA)"));
     }
 
-    
     @Test
     public void Id_12001_ok(@TempDir Path tempDir) throws Exception {
         String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
@@ -52,5 +52,150 @@ public class Leitung {
         assertFalse(content.contains("Warning"));
         assertFalse(content.contains("Error"));
     }
+    
+    @Test
+    public void Id_12002_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12002/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12002/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12002/12002_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 23: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002001: MANDATORY BaulicherZustand"));
+    }
+
+    @Test
+    public void Id_12002_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12002/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12002/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12002/12002_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }
+    
+    @Test
+    public void Id_12004_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12004/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12004/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12004/12004_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 23: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002001: MANDATORY BetreiberRef"));
+    }
+
+    @Disabled
+    @Test
+    public void Id_12004_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12004/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12004/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12004/12004_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }
+
+    @Test
+    public void Id_12007_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12007/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12007/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12007/12007_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 23: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002001: MANDATORY Finanzierung AND != unbekannt"));
+        assertTrue(content.contains("Error: line 32: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002002: MANDATORY Finanzierung AND != unbekannt"));
+    }
+
+    @Test
+    public void Id_12007_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12007/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12007/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12007/12007_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }
+    
+    @Test
+    public void Id_12008_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12008/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12008/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12008/12008_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 23: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002001: MANDATORY FunktionHierarchisch AND != unbekannt"));
+        assertTrue(content.contains("Error: line 32: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002002: MANDATORY FunktionHierarchisch AND != unbekannt"));
+        assertTrue(content.contains("Error: line 42: VSADSSMINI_2020_LV95.VSADSSMini.Leitung: tid deg5mQXX20002003: MANDATORY FunktionHierarchisch AND != unbekannt"));
+    }
+
+    @Test
+    public void Id_12008_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"leitung/12008/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"leitung/12008/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"leitung/12008/12008_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }
+
 
 }
