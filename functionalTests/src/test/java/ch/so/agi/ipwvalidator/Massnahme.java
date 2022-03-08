5bd57a394d5f37d525960bda84c9ae34069926da
edigonzales <edi.gonzales@gmail.com>
2022-03-08 18:26:02 +0100

    start with massnahme

---
+++ functionalTests/src/test/java/ch/so/agi/ipwvalidator/Massnahme.java
@@ -0,0 +1,7 @@
+package ch.so.agi.ipwvalidator;
+
+public class Massnahme {
+    private static final String TEST_IN = "src/test/data/";
+    private static final String LOGFILE_NAME = "ilivalidator.log";
+
+}
