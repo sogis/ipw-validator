04ebb6192bdccfc8d370316613610ff74677289b
edigonzales <edi.gonzales@gmail.com>
2022-03-09 11:20:35 +0100

    fix knoten test

--- functionalTests/src/test/java/ch/so/agi/ipwvalidator/Massnahme.java
+++ functionalTests/src/test/java/ch/so/agi/ipwvalidator/Massnahme.java
@@ -51,5 +51,41 @@ public class Massnahme {
         assertFalse(content.contains("Warning"));
         assertFalse(content.contains("Error"));
     }
+    
+    @Test
+    public void Id_13003_fail(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"massnahme/13003/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"massnahme/13003/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"massnahme/13003/13003_fail.xtf", settings);
+        assertFalse(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertTrue(content.contains("Error: line 23: VSADSSMINI_2020_LV95.VSADSSMini.Massnahme: tid deg5mQXX20004001: MANDATORY Datum_Eingang"));
+    }
+
+    @Test
+    public void Id_13003_ok(@TempDir Path tempDir) throws Exception {
+        String logFileName = Paths.get(tempDir.toFile().getAbsolutePath(), LOGFILE_NAME).toFile().getAbsolutePath();
+        
+        Settings settings = new Settings();
+        settings.setValue(Validator.SETTING_LOGFILE, logFileName);
+        settings.setValue(Validator.SETTING_ILIDIRS, TEST_IN+"models/;"+TEST_IN+"massnahme/13003/");
+        settings.setValue(Validator.SETTING_CONFIGFILE, TEST_IN+"massnahme/13003/config.toml");
+        
+        boolean valid = Validator.runValidation(TEST_IN+"massnahme/13003/13003_ok.xtf", settings);
+        assertTrue(valid);
+
+        String content = new String(Files.readAllBytes(Paths.get(logFileName)));
+        //System.out.println(content);
+        assertFalse(content.contains("Warning"));
+        assertFalse(content.contains("Error"));
+    }
+
 
 }
